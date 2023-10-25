89b3914b14db950ebe6562a34043af3cc617516c
ianvictor <ianvictorandrade@hotmail.com>
2023-10-25 10:32:22 -0400

    initial commit

---
+++ organoreactjs/src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+function Banner() {
+    // JSX
+    return (
+        <header className={"banner"}>
+        <img src="/imagens/banner.png" alt="logo do banner"/>
+        </header>
+    );
+}
+
+export default Banner;
