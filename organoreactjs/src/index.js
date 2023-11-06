0ef762910ef920fa80f3358b04dc27fd2907d6ad
ianvictor <ianvictorandrade@hotmail.com>
2023-11-06 10:24:27 -0400

    Formulário pronto

--- organoreactjs/src/index.js
+++ organoreactjs/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Banner from "./componentes/Banner/Banner";
-import CampoTexto from "./componentes/CampoTexto";
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Banner />
-      <CampoTexto />
+    <App />
   </React.StrictMode>
 );
 
